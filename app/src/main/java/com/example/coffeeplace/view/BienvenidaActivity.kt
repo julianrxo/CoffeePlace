4aa788a83f0877a2a29128b1f289922ae794875e
Julián <julianxdev@gmail.com>
2025-11-20 00:32:02 -0500

    Funcionalidades finales - APK V.3

--- app/src/main/java/com/example/coffeeplace/view/BienvenidaActivity.kt
+++ app/src/main/java/com/example/coffeeplace/view/BienvenidaActivity.kt
@@ -14,7 +14,7 @@ class BienvenidaActivity : AppCompatActivity() {
         // Referencia al botón "Comenzar"
         val btnGoLogin = findViewById<Button>(R.id.btnGoLogin)
 
-        // Al hacer clic, pasamos al LoginActivity
+        // Al hacer clic pasamos al LoginActivity
         btnGoLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
